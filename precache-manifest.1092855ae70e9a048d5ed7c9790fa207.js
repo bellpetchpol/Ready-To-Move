1b0e041e15e1864fae875d6c126e53c589e0809f
bigbell_MacPro2018 <bell_2530@hotmail.com>
2018-08-24 08:58:40 +0700

    first commit

---
+++ precache-manifest.1092855ae70e9a048d5ed7c9790fa207.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0195fa77d54e22a1fe6e",
+    "url": "/js/chunk-vendors.67952a44.js"
+  },
+  {
+    "revision": "8b96a39f0bb05419595f",
+    "url": "/js/app.cf4e1e9e.js"
+  },
+  {
+    "revision": "fceeb5487e86cd29ba36",
+    "url": "/js/about.5f7d6f63.js"
+  },
+  {
+    "revision": "363b8a7dbc5e06ff30cb82a026005a81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0195fa77d54e22a1fe6e",
+    "url": "/css/chunk-vendors.bd48805f.css"
+  },
+  {
+    "revision": "8b96a39f0bb05419595f",
+    "url": "/css/app.547b115f.css"
+  }
+];
